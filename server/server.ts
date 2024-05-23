200d188de9eab85132acaf342f3e7e7ce8e05621
Jeevan Miranda <158778751+jeevan-miranda@users.noreply.github.com>
2024-05-24 10:13:08 +1200

    stretch 1 router added

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,7 @@ import * as Path from 'node:path'
 import express from 'express'
 import cors, { CorsOptions } from 'cors'
 import cats from './routes/cats.ts'
+import multipleCats from './routes/multipleCats.ts'
 
 const server = express()
 
@@ -15,6 +16,7 @@ server.get('/api/v1/greeting', (req, res) => {
 server.use(express.json())
 server.use(cors('*' as CorsOptions))
 server.use('/api/v1/cats', cats)
+server.use('/api/v1/multipleCats', multipleCats)
 
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
