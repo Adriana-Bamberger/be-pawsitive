82aa9ddf7d01bf0072767406702f513c0fea1616
Adriana Bamberger <162661198+Adriana-Bamberger@users.noreply.github.com>
2024-05-24 09:51:03 +1200

    added router

---
+++ server/routes/cats.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import request from 'superagent'
+import 'dotenv/config'
+
+const router = express.Router()
+const API_KEY = process.env.CAT_API_KEY
+
+router.get('/', async (req, res) => {
+  try {
+    const result = await request
+      .get('https://api.thecatapi.com/v1/images/search')
+      .query({ limit: 1, api_key: API_KEY })
+    res.json(result.body)
+  } catch (err) {
+    if (err instanceof Error) {
+      res.status(500).send((err as Error).message)
+    } else {
+      res.status(500).send('Something went wrong')
+    }
+  }
+})
+
+export default router
